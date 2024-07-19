0c3a27cc10e6b688786d814b6cd160e4efded624
NicolasFG <nicolas.figueroa@utec.edu.pe>
2024-07-18 20:47:16 -0500

    Ultimos cambios

--- back/app.py
+++ back/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import pickle
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+CORS(app)  # Habilita CORS para toda la aplicación
 
 # Cargar modelo
 with open('modelo_precargado.pkl', 'rb') as f:
@@ -95,4 +99,4 @@ def recomendar():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
